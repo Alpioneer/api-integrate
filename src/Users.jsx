050a4c7a6e4a57c9bc59add1d683f5eff382356b
yeonkor <alpioneer92@Gmail.com>
2021-04-20 23:01:25 +0900

    ㅋㅋㅋㅋㅋㅋㅋㅋㅋㅋ

--- src/Users.jsx
+++ src/Users.jsx
@@ -6,43 +6,45 @@ const Users=()=>{
 
 
     const [users,setUsers]=useState(null);
-    const [loading, setloading] =useState(false);
+    const [loading, setLoading] =useState(false);
     const [error,setError] = useState(null);
+
+    
+    const fetchUsers = async()=>{
+
+        try{
+
+            //요청이 시작 할 때에는 error와 users를 초기화,
+
+            setError(null);
+            setUsers(null);
+
+            //loading 상태를 true로 바꿈
+
+            setLoading(true);
+
+            const response = await axios.get(
+
+                'https://jsonplaceholder.typicode.com/users'
+
+            );
+
+
+            setUsers(response.data);
+
+
+        }  catch(e){
+
+            setError(e);
+        }
+
+        setLoading(false);
+     };
+
+
 
 
     useEffect(()=>{
-
-        const fetchUsers = async()=>{
-
-            try{
-
-                //요청이 시작 할 때에는 error와 users를 초기화,
-
-                setError(null);
-                setUsers(null);
-
-                //loading 상태를 true로 바꿈
-
-                setloading(true);
-
-                const response = await axios.get(
-
-                    'https://jsonplaceholder.typicode.com/users'
-
-                );
-
-
-                setUsers(response.data);
-
-
-            }  catch(e){
-
-                setError(e);
-            }
-
-            setloading(false);
-         };
-
 
          fetchUsers();
 
@@ -55,12 +57,21 @@ const Users=()=>{
 
 
     return(
-    
+        
+        
+        <div>
     <ul>
 
     {users.map(user => (<li key={user.id}>{user.username} ({user.name}) </li>))}
 
+
     </ul>
+
+    <button onClick={fetchUsers}>다시 불러오기</button>
+
+
+    </div>
+    
     )
 
 
